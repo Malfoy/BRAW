8127a27368ea5bf2980446d979326598b2bb758e
Malfoy <antoine.limasset@gmail.com>
2017-06-06 17:25:05 +0200

    better visualization added a warning empty file

--- N50.cpp
+++ N50.cpp
@@ -10,6 +10,21 @@
 using namespace std;
 
 
+string intToString(uint64_t n){
+	if(n<1000){
+		return to_string(n);
+	}
+	string end(to_string(n%1000));
+	if(end.size()==3){
+		return intToString(n/1000)+","+end;
+	}
+	if(end.size()==2){
+		return intToString(n/1000)+",0"+end;
+	}
+	return intToString(n/1000)+",00"+end;
+}
+
+
 
 int main(int argc, char ** argv){
 	if(argc<2){
@@ -20,6 +35,10 @@ int main(int argc, char ** argv){
 	srand (time(NULL));
 	string ref, useless;
 	ifstream in(input);
+	if(not in){
+		cout<<"Problem opening file"<<endl;
+		return 0;
+	}
 	vector<uint> lengths;
 	uint size(0);
 	while(not in.eof()){
@@ -36,22 +55,22 @@ int main(int argc, char ** argv){
 		total+=lengths[i];
 		++i;
 	}
-	cout<<"#contigs: "<<lengths.size()<<endl;
-	cout<<"Total size: "<<size<<endl;
-	cout<<"N50: "<<lengths[i-1]<<endl;
-	cout<<"L50: "<<i<<endl;
+	cout<<"#contigs: "<<intToString(lengths.size())<<endl;
+	cout<<"Total size: "<<intToString(size)<<endl;
+	cout<<"N50: "<<intToString(lengths[i-1])<<endl;
+	cout<<"L50: "<<intToString(i)<<endl;
 	total=i=0;
 	while(total<size*0.80){
 		total+=lengths[i];
 		++i;
 	}
-	cout<<"N80: "<<lengths[i-1]<<endl;
-	cout<<"L80: "<<i<<endl;
+	cout<<"N80: "<<intToString(lengths[i-1])<<endl;
+	cout<<"L80: "<<intToString(i)<<endl;
 	total=i=0;
 	while(total<size*0.90){
 		total+=lengths[i];
 		++i;
 	}
-	cout<<"N90: "<<lengths[i-1]<<endl;
-	cout<<"L90: "<<i<<endl;
+	cout<<"N90: "<<intToString(lengths[i-1])<<endl;
+	cout<<"L90: "<<intToString(i)<<endl;
 }
