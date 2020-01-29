64344d241cbc437004533b86c02a0911a21edb9e
Malfoy <antoine.limasset@gmail.com>
2020-01-29 11:11:25 +0100

    faster paired simu

--- pairedSimulator.cpp
+++ pairedSimulator.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <string>
 #include <iostream>
+#include "omp.h"
 
 
 
@@ -15,6 +16,8 @@ static unsigned int seed;
 uint32_t xs(uint32_t& y){
     y^=(y<<13); y^=(y>>17);y=(y^=(y<<15)); return y;
 }
+
+
 
 
 
@@ -36,6 +39,9 @@ string revComp(const string& s){
     }
     return rc;
 }
+
+
+
 
 
 
@@ -70,7 +76,7 @@ char randNucle(char c){
 
 int main(int argc, char ** argv){
     if(argc<7){
-        cout<<"[Genome reference file] [read length] [fragment length] [coverage] [error rate] [prefix] [MP or PE]"<<endl;
+        cout<<"[Genome reference file] [read length] [fragment length] [coverage] [error rate] [prefix] [MP or PE] [perfect]"<<endl;
         exit(0);
     }
     string input(argv[1]);
@@ -83,89 +89,118 @@ int main(int argc, char ** argv){
     string prefix(argv[6]);
     string type(argv[7]);
     bool matePair(false);
+    bool perfect(false);
     if(type=="MP"){
         matePair=true;
         cout<<"I produce mate pairs"<<endl;
     }else{
         cout<<"I produce paired ends"<<endl;
     }
-    string useless, ref,read,pread;
+    if(argc>8){
+        cout<<"I produce erroless reads"<<endl;
+        perfect=true;
+    }
+
     uint i(0);
-    ofstream perfect("p."+prefix+".fa"),out(prefix+".fa");
+    ofstream out(prefix+".fa");
+    ofstream outperfect;
+    if(perfect){
+        outperfect.open("p."+prefix+".fa");
+    }
 
-    while(not in.eof()){
-        seed=(rand());
-        getline(in,useless);
-        getline(in,ref);
-        if(ref.size()<fragmentSize){
-            break;
-        }
-        if(not ref.empty() and not useless.empty()){
-            uint64_t nucProduced(0);
-            while(nucProduced<coverage*ref.size()){
-                if(i%100==0){
-                    seed=(rand());
-                }
-                //produce a read
-                uint position=xs(seed)%ref.size();
-                if(position+fragmentSize<=ref.size()){
-                    bool valid(true);
-                    uint error(0);
+    #pragma omp parallel
+    {
+        string useless, ref,read,pread;
+        while(not in.eof()){
+            seed=(rand());
+            #pragma omp critical (inputfile)
+            {
+                getline(in,useless);
+                getline(in,ref);
+            }
+            if(ref.size()<fragmentSize){
+                break;
+            }
+            if(not ref.empty() and not useless.empty()){
+                uint64_t nucProduced(0);
+                while(nucProduced<coverage*ref.size()){
+                    if(i%1000==0){
+                        seed=(rand());
+                    }
+                    //produce a read
+                    uint position=xs(seed)%ref.size();
+                    if(position+fragmentSize<=ref.size()){
+                        bool valid(true);
+                        uint error(0);
 
-                    pread=ref.substr(position,length);
-                    read=pread;
-                    for(uint i(0);i<read.size();++i){
-                        if(read[i]=='N'){valid=false;break;}
-                        if(xs(seed)%errorRate==0){
-                            read[i]=randNucle(read[i]);
-                            ++error;
+                        pread=ref.substr(position,length);
+                        read=pread;
+                        for(uint i(0);i<read.size();++i){
+                            if(read[i]=='N'){valid=false;break;}
+                            if(xs(seed)%errorRate==0){
+                                read[i]=randNucle(read[i]);
+                                ++error;
+                            }
                         }
-                    }
-                    if(valid){
-                        //~ perfect<<">"+to_string(i)<<" "<<position<<endl;
-                        //~ perfect<<pread<<endl;
-                        out<<">"+to_string(i)<<"_"<<position<<"\n";
-                        perfect<<">"+to_string(i)<<"_"<<position<<"\n";
-                        if(matePair){
-                            out<<revComp(read)<<"\n";
-                            perfect<<revComp(pread)<<"\n";
-                        }else{
-                            out<<read<<"\n";
-                            perfect<<(pread)<<"\n";
+                        if(valid){
+                            #pragma omp critical (outfile)
+                            {
+                                out<<">"+to_string(i)<<"_"<<position<<"\n";
+                                if(perfect){
+                                    outperfect<<">"+to_string(i)<<"_"<<position<<"\n";
+                                }
+                                if(matePair){
+                                    out<<revComp(read)<<"\n";
+                                    if(perfect){
+                                        outperfect<<revComp(pread)<<"\n";
+                                    }
+                                }else{
+                                    out<<read<<"\n";
+                                    if(perfect){
+                                        outperfect<<(pread)<<"\n";
+                                    }
+                                }
+                            }
+                            nucProduced+=read.size();
+                            //~ ++i;
                         }
-                        nucProduced+=read.size();
-                        //~ ++i;
-                    }
 
-                    valid=(true);
-                    error=(0);
-                    pread=ref.substr(position+fragmentSize-length,length);
-                    read=pread;
-                    for(uint i(0);i<read.size();++i){
-                        if(read[i]=='N'){valid=false;break;}
-                        if(xs(seed)%errorRate==0){
-                            read[i]=randNucle(read[i]);
-                            ++error;
+                        valid=(true);
+                        error=(0);
+                        pread=ref.substr(position+fragmentSize-length,length);
+                        read=pread;
+                        for(uint i(0);i<read.size();++i){
+                            if(read[i]=='N'){valid=false;break;}
+                            if(xs(seed)%errorRate==0){
+                                read[i]=randNucle(read[i]);
+                                ++error;
+                            }
                         }
-                    }
-                    if(valid){
-                        //~ perfect<<">"+to_string(i)<<"bis "<<(position+fragmentSize-length)<<endl;
-                        //~ perfect<<pread<<endl;
-                        out<<">"+to_string(i)<<"bis_"<<(position+fragmentSize-length)<<"\n";
-                        perfect<<">"+to_string(i)<<"bis_"<<(position+fragmentSize-length)<<"\n";
-                        if(not matePair){
-                            out<<revComp(read)<<"\n";
-                            perfect<<revComp(pread)<<"\n";
-                        }else{
-                            out<<read<<"\n";
-                            perfect<<(pread)<<"\n";
+                        if(valid){
+                            #pragma omp critical (outfile)
+                            {
+                                out<<">"+to_string(i)<<"bis_"<<(position+fragmentSize-length)<<"\n";
+                                if(perfect){
+                                    outperfect<<">"+to_string(i)<<"bis_"<<(position+fragmentSize-length)<<"\n";
+                                }
+                                if(not matePair){
+                                    out<<revComp(read)<<"\n";
+                                    if(perfect){
+                                        outperfect<<revComp(pread)<<"\n";
+                                    }
+                                }else{
+                                    out<<read<<"\n";
+                                    if(perfect){
+                                        outperfect<<(pread)<<"\n";
+                                    }
+                                }
+                            }
+                            nucProduced+=read.size();
+                            ++i;
                         }
-                        nucProduced+=read.size();
-                        ++i;
                     }
                 }
             }
         }
     }
-
 }
