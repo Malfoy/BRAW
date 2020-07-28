baac671648fa1e9831e1f851e8b4920cf1b46391
Malfoy <antoine.limasset@gmail.com>
2020-07-28 15:50:33 +0200

    correct multithread

--- randomSeq.cpp
+++ randomSeq.cpp
@@ -8,38 +8,18 @@
 using namespace std;
 
 
-static uint32_t seed;
+
+
 
 uint32_t xs(uint32_t& y){
 	y^=(y<<13); y^=(y>>17);y=(y^=(y<<15)); return y;
 }
 
 
-char randNucle(char c){
-	switch (xs(seed)%4){
-		case 0:
-			if(c!='A'){
-				return 'A';
-			}
-		case 1:
-			if(c!='C'){
-				return 'C';
-			}
-		case 2:
-			if(c!='G'){
-				return 'G';
-			}
-		case 3:
-			if(c!='T'){
-				return 'T';
-			}
-	}
-	return randNucle(c);
-}
 
 
 
-char randNuc(){
+char randNuc(uint32_t& seed){
 	switch (xs(seed)%4){
 		case 0:
 				return 'A';
@@ -55,10 +35,10 @@ char randNuc(){
 
 
 
-string random_sequence(uint64_t basesnumber,string& res){
+string random_sequence(uint64_t basesnumber,string& res,uint32_t& seed){
   res.clear();
   for(uint i(0);i<basesnumber;++i){
-    res.push_back(randNuc());
+    res.push_back(randNuc(seed));
   }
   return res;
 }
@@ -73,16 +53,15 @@ int main(int argc, char ** argv){
 	}
 	uint64_t basesnumber(stoi(argv[1]));
 	uint64_t seqnumber(stoi(argv[2]));
-
+	srand (time(NULL));
 	
 	uint i(0);
 	#pragma omp parallel for
 	for(i=0;i<seqnumber/1000;++i){
 		string rs;
-		srand (time(NULL));
-		seed=(rand());
+		uint32_t seed=(rand());
 		for(int j(0);j<1000;++j){
-			rs=random_sequence(basesnumber,rs);
+			rs=random_sequence(basesnumber,rs,seed);
 			#pragma omp critical
 			{
 				cout<<">1\n"<<rs<<'\n';
