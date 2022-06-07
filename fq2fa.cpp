5ba954b4b4283dbac9368bb60d9c00a52d6e12fc
malfoy <antoine.limasset@gmail.com>
2022-06-07 10:17:26 +0200

    fq2fa zip and clean

--- fq2fa.cpp
+++ fq2fa.cpp
@@ -4,10 +4,33 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include "zstr.hpp"
+
 
 
 
 using namespace std;
+
+
+
+void clean(string& str){
+	for(uint i(0); i< str.size(); ++i){
+		switch(str[i]){
+			case 'a':break;
+			case 'A':break;
+			case 'c':break;
+			case 'C':break;
+			case 'g':break;
+			case 'G':break;
+			case 't':break;
+			case 'T':break;
+			case 'N':break;
+			case 'n':break;
+			default: str.clear();return;
+		}
+	}
+	transform(str.begin(), str.end(), str.begin(), ::toupper);
+}
 
 
 
@@ -16,10 +39,19 @@ int main(int argc, char ** argv){
 	if(argc<2){
 		streaming=true;
 	}
+	bool cleaning(false),headercleaning(false);
+	if(argc>2){
+		if(string(argv[2])=="1" or string(argv[2])=="3"){
+			cleaning=true;
+		}
+		if(string(argv[2])=="2" or string(argv[2])=="3"){
+			headercleaning=true;
+		}
+	}
 	if(not streaming){
 		string input(argv[1]);
 		string ref, useless,header;
-		ifstream in(input);
+		zstr::ifstream in(input);
 		while(not in.eof()){
 			getline(in,header);
 			getline(in,ref);
@@ -27,11 +59,17 @@ int main(int argc, char ** argv){
 			getline(in,useless);
 			if(header.size()>1){
 				if(ref.size()>1){
-					cout<<">"+header.substr(1)<<"\n";
+					if(headercleaning){
+						cout<<">A"<<endl;
+					}else{
+						cout<<">"+header.substr(1)<<"\n";
+					}
+					if(cleaning){
+						clean(ref);
+					}
 					cout<<ref<<"\n";
 				}else{
-					cout<<">"+header.substr(1)<<"\n";
-					cout<<ref<<"\n";
+					
 				}
 			}
 		}
